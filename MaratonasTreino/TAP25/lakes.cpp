e06b7b001823dad2305ac220ec2bc316a504d2f9
alvarotig <alvarotardin@gmail.com>
2026-09-11 17:25:11 -0300

    identação lakes

--- MaratonasTreino/TAP25/lakes.cpp
+++ MaratonasTreino/TAP25/lakes.cpp
@@ -6,7 +6,7 @@ int n, x;
 lli g;
 vector<lli> v;
 
-//a logica é f (k,i) -> f(k, i+1) || f(k+1, i+k) + G - soma(i até i+k-1) 
+//a logica é f (k,i) ->( f(k, i+1) || f(k+1, i+k) + G - soma(i até i+k-1) ) 
 
 lli solve (vector<vector<lli>>& dp, vector<lli>& pref, int k, int i)
 {
@@ -15,7 +15,14 @@ lli solve (vector<vector<lli>>& dp, vector<lli>& pref, int k, int i)
 
     lli pula = solve(dp, pref, k, i+1), s; // f(k, i+1);
 
-    if (i+k-1 < n) s = pref[i+k-1] - (i > 0 ? pref[i-1] : 0);
+    if (i+k-1 < n)
+    {
+        int temp;
+        if (i > 0) temp = pref[i-1]
+        else temp = 0;
+        s = pref[i+k-1] - temp;  
+    } 
+
     else return dp[k][i] = pula;
 
     lli coloca = solve(dp, pref, k+1, i+k) + g - s; // f(k+1, i+k+1)
@@ -28,7 +35,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> n >> g;
-    vector<lli> pref(n); // guarda soma ate ali
+    vector<lli> pref(n); 
 
     lli t; cin >> t; v.push_back(t); pref[0] = t;
     for (int i = 1; i < n; i++)
